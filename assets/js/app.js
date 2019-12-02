e470c5bedf7388f9ef8fee01a1b36658c2f9ab9d
Analin Flores <foglzerika@gmail.com>
2019-12-02 11:31:01 -0600

    Creating user dinamically

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,49 @@
+//global variables
+let arrayUsers = [];
+let imageArray  = [
+        {"src":"./assets/img/profile.png"}
+
+]
 class User{
     constructor(name, nickName, profilePicture){
         this.name = name ;
-        this.nickName = nickname;
+        this.nickName = nickName;
         this.profilePicture = profilePicture;
     }
     printName(){
+
         console.log(`Hello ${this.name}` );
     }
 }
+//functions
+function printProfiles(){
+    document.createElement(div);
+    
+}
 
-let user1 = new User( "Antonio" , "ant" , "img");
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let html = '';
+    arrayUsers.forEach(user => {
+       // var ruta= arregloImagenes[posicion].src;
+
+      html += `<div class="card col-md-3" data-card-name="${user.name}">`;
+      html += `<div class="card-text">${user.name}</div>`;
+      html+= `<img  class="card-img-top" src =${imageArray[0].src}>`
+      html += `</div>`;
+    });
+
+ // Add all the divs to the HTML
+ document.querySelector('.containerProfiles').innerHTML = html;
+})
+
+let user1 = new User( "Antonio" , "ant" , "./assets/profile.png");
 let user2 = new User("Arely" , "Are" , "Img");
 let user3  = new User ("Erika" , "Anie" , "ImgUrl");
+
+arrayUsers.push(user1);
+
+arrayUsers.push(user2);
+
+arrayUsers.push(user3);
+
+console.log(arrayUsers);
